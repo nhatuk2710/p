5ce57b584250e274df9f6dac988ce54e86e55d7c
nhatuk2710 <49880246+nhatuk2710@users.noreply.github.com>
2020-03-27 16:15:59 +0700

    Update layout.blade.php

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -118,7 +118,7 @@
                 <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
                     <h4 class="page-title">Dashboard</h4> </div>
                 <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-                    <a href="{{url("admin/category/brandcreate")}}" target="_blank" class="btn btn-danger
+                    <a href="{{url("admin/category/created")}}" target="_blank" class="btn btn-danger
                     pull-right m-l-20 hidden-xs hidden-sm waves-effect waves-light">Add to cart</a>
                     <ol class="breadcrumb">
                         <li><a href="#">Dashboard</a></li>
